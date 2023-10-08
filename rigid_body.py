03d9c7d299aff5f82dbd717a6b677db00cb2597b
Tim Liu <103532834+TinyTim4@users.noreply.github.com>
2023-10-08 19:30:12 -0400

    Update rigid_body.py

--- rigid_body.py
+++ rigid_body.py
@@ -27,33 +27,35 @@ class CarController:
     def on_key_press(self, event):
         # Define step size for arrow key movement
         step = 0.01
-        prev = ["x", 0]
+        prev = ["x", 0, 0]
         if event.key == 'up':
             if check_boundary(self.car,1,2):
-                self.car.set_y(self.car.get_y() + step)
-                prev = ["y", step*-1]
+                y = step * math.sin(math.radians(self.degrees()))
+                self.car.set_y(self.car.get_y() + y)
+                x = step * math.cos(math.radians(self.degrees()))
+                self.car.set_x(self.car.get_x() + x)
+                prev = ["f", x, y]
         elif event.key == 'down':
             if check_boundary(self.car, 1,0):
-                self.car.set_y(self.car.get_y() - step)
-                prev = ["y", step]
-        elif event.key == 'right':
-            if check_boundary(self.car, 0,2):
-                self.car.set_x(self.car.get_x() + step)
-                prev = ["x", step*-1]
+                y = step * math.sin(math.radians(self.degrees()))
+                self.car.set_y(self.car.get_y() - y)
+                x = step * math.cos(math.radians(self.degrees()))
+                self.car.set_x(self.car.get_x() - x)
+                prev = ["b", x, y]
         elif event.key == 'left':
-            if check_boundary(self.car, 0,0):
-                self.car.set_x(self.car.get_x() - step)
-                prev = ["x", step]
-        elif event.key == 'a':
             self.car.set(angle = self.degrees() + 10)
-            prev = ["a", -10]
-        elif event.key == 'd':
+            prev = ["a", -10, 0]
+        elif event.key == 'right':
             self.car.set(angle = self.degrees() - 10)
-            prev = ["a", 10]
+            prev = ["a", 10, 0]
         if(not check_car(self.car, self.obstacles)): 
-            if prev[0] == "x": self.car.set_x(self.car.get_x() + prev[1]) 
+            if prev[0] == "f": 
+                self.car.set_x(self.car.get_x() - prev[1]) 
+                self.car.set_y(self.car.get_y() - prev[2]) 
             elif prev[0] == "a": self.car.set(angle = self.degrees() + prev[1])
-            else: self.car.set_y(self.car.get_y() + prev[1]) 
+            else: 
+                self.car.set_x(self.car.get_x() + prev[1]) 
+                self.car.set_y(self.car.get_y() + prev[2]) 
             
         # Update the car's position
         self.fig.canvas.draw()
