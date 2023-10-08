034348cfd51983b15fbb69d8473bafb71f38b891
Sina-Haz <101819919+Sina-Haz@users.noreply.github.com>
2023-10-08 13:12:00 -0400

    writing on-key

--- planar_arm.py
+++ planar_arm.py
@@ -1,6 +1,7 @@
 from create_scene import create_plot, make_polygons, show_scene, add_polygon_to_scene
 import matplotlib.patches as patches
-from math import cos, sin, degrees, sqrt, pi
+from numpy import cos, sin, degrees, pi, radians
+import matplotlib.pyplot as plt
 
 # This class is used to control the planar arm using keyboard input
 class Arm_Controller:
@@ -28,21 +29,22 @@ class Arm_Controller:
         return(x,y)
     
 
-    def __init__(self,theta1,theta2):
+    def __init__(self,theta1,theta2,ax):
+        self.ax = ax
         self.joint1 = (1,1) #Center of the first circle
         self.rad = 0.05 #radius of all joints
         self.rwid = 0.1 #width of both rectangles
         self.rlen1 = 0.4 #Length of first rectangle
         self.rlen2 = 0.25 #Length of 2nd rectangle
         self.theta1 = theta1 #First theta value
+        self.theta2 = theta2
         self.anchor1 = Arm_Controller.compute_rect_anchor(self.theta1, self.joint1, self.rad, self.rwid) #Anchor of 1st rectangle
         self.joint2 = Arm_Controller.compute_circle_center(self.theta1, self.joint1, self.rad, self.rlen1)
-        self.theta2 = theta2
         self.anchor2 = Arm_Controller.compute_rect_anchor(self.theta2, self.joint2, self.rad, self.rwid)
         self.joint3 = Arm_Controller.compute_circle_center(self.theta2,self.joint2,self.rad, self.rlen2)
 
 
-    def draw_arm(self, ax):
+    def draw_arm(self):
         joint1 = patches.Circle(self.joint1, self.rad, fill=True, color='b')
         rect1 = patches.Rectangle(self.anchor1,self.rwid,self.rlen1, fill=True,color='g')
         rect1.set_angle(degrees(self.theta1 - pi/2))
@@ -50,19 +52,47 @@ class Arm_Controller:
         rect2 = patches.Rectangle(self.anchor2,self.rwid, self.rlen2, fill=True,color='g')
         rect2.set_angle(degrees(self.theta2 - pi/2))
         joint3 = patches.Circle(self.joint3,self.rad,fill=True,color='b')
-        ax.add_patch(joint1)
-        ax.add_patch(rect1)
-        ax.add_patch(joint2)
-        ax.add_patch(rect2)
-        ax.add_patch(joint3)
-        show_scene(ax)
+        self.ax.add_patch(joint1)
+        self.ax.add_patch(rect1)
+        self.ax.add_patch(joint2)
+        self.ax.add_patch(rect2)
+        self.ax.add_patch(joint3)
+        show_scene(self.ax)
+
+    # Call this function when angles are changed to recompute the position of the anchors and joints
+    def re_orient(self):
+        self.anchor1 = Arm_Controller.compute_rect_anchor(self.theta1, self.joint1, self.rad, self.rwid)
+        self.joint2 = Arm_Controller.compute_circle_center(self.theta1, self.joint1, self.rad, self.rlen1)
+        self.anchor2 = Arm_Controller.compute_rect_anchor(self.theta2, self.joint2, self.rad, self.rwid)
+        self.joint3 = Arm_Controller.compute_circle_center(self.theta2,self.joint2,self.rad, self.rlen2)
+
+
+    def on_key(self, event):
+        if event.key == 'left':
+            self.theta1 -= radians(5)
+        elif event.key == 'right':
+            self.theta1 += radians(5)
+        elif event.key == 'up':
+            self.theta2 += radians(5)
+        elif event.key == 'down':
+            self.theta2 -= radians(5)
+        
+        # Clear the current axis and redraw the arm
+        self.ax.cla()
+        self.re_orient()
+        self.draw_arm()
+        self.ax.figure.canvas.draw()
+        
 
 
 
 
 if __name__ == '__main__':
-    arm = Arm_Controller(0, pi/2)
-    ax = create_plot()
-    arm.draw_arm(ax)
+    fig,ax = plt.subplots(dpi=100)
+    arm = Arm_Controller(radians(30), radians(-30),ax)
+    arm.ax.figure.canvas.mpl_connect('key_press_event', arm.on_key)
+    arm.draw_arm()
+    show_scene(arm.ax)
+
     
     
